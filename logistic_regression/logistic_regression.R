95c7f04f1757ae5a87815ab1a4841abcc72a80df
Krzysztof Krenc <31450069+Hravan@users.noreply.github.com>
2018-04-16 20:33:03 +0200

    Added function to perform gradient descent algorithm for given hypothesis function

--- logistic_regression/logistic_regression.R
+++ logistic_regression/logistic_regression.R
@@ -11,7 +11,7 @@ sigmoid <- function(z) {
   1 / (1 + e ^ (-z))
 }
 
-hypothesis <- function(theta, X) {
+hyp_log_reg <- function(theta, X) {
   # Computes a value of hypothesis function for logistic regression.
   #
   # Args:
@@ -20,6 +20,7 @@ hypothesis <- function(theta, X) {
   #
   # Returns:
   #   Vector of values of hypothesis for all Xes.
+  
   as.vector(sigmoid(t(theta) %*% t(X)))
 }
 
@@ -33,7 +34,8 @@ cost_success <- function(theta, X) {
   #
   # Returns:
   #   double representing the cost of using given parameters for the input of sigmoid function.
-  -log(hypothesis(theta, X))
+  
+  -log(hyp_log_reg(theta, X))
 }
 
 cost_failure <- function(theta, X) {
@@ -47,7 +49,7 @@ cost_failure <- function(theta, X) {
   # Returns:
   #   double representing the cost of using given parameters for the input of sigmoid function.
   
-  -log(1 - hypothesis(theta, X))
+  -log(1 - hyp_log_reg(theta, X))
 }
 
 cost <- function(theta, X, y) {
@@ -60,5 +62,45 @@ cost <- function(theta, X, y) {
   #
   # Returns:
   #   Cost of logistic regression function for given value of theta.
+  
   1 / nrow(X) * sum(t(y) %*% cost_success(theta, X) + t(1 - y) %*% cost_failure(theta, X))
+}
+
+res <- function(theta, X, y, hyp_fun) {
+  # Compute residuals for given hypothesis function for given values of theta
+  #
+  # Args:
+  #   theta:   vectof of coefficients
+  #   X:       [m X n] matrix of m observations for n independent variables
+  #   y:       vector of m observations for binary (0, 1) dependent variable
+  #   hyp_fun: model function used to approximate values in y for given X
+  #
+  # Returns:
+  #   Vector of m values of residuals.
+  as.vector(hyp_fun(theta, X) - y)
+}
+
+gradient_descent <- function(theta, X, y, alpha, num_iters, hyp_fun) {
+  # Perform num_terations of gradient descent algorithm to find values for theta that minimize
+  # cost function for given X and y
+  #
+  # Args:
+  #   theta:         vector of initial values of coefficients
+  #   X:             matrix [m X n] of m observations for n dependent variables
+  #   y:             vector of m observations for independent variable (only zeros and ones)
+  #   alpha:         learning rate
+  #   num_iters:     number of iterations of the algorithm
+  #   hyp_function:  function for which the values of theta are being looked for
+  #
+  # Returns:
+  #   list:
+  #     theta: values for theta that minimize the cost function
+  #     cost_vals: vector of num_iters cost values, one for every iteration of cost function
+  c_vals <- numeric(num_iters)
+  for (i in 1:num_iters) {
+    c_vals[i] <- cost(theta, X, y)
+    theta <- theta - alpha / nrow(X) * t(X) %*% res(theta, X, y, hyp_fun)
+  }
+  
+  list(theta = theta, cost_vals = c_vals)
 }
--- logistic_regression/test_logistic_regression.R
+++ logistic_regression/test_logistic_regression.R
@@ -46,7 +46,7 @@ test_that('Hypothesis for logistic regression is computed properly for one varia
   theta <- c(0, 0)
   expected <- sigmoid(t(theta) %*% t(X))
   expect_equal(as.vector(expected),
-               hypothesis(theta, X))
+               hyp_log_reg(theta, X))
 })
 
 test_that('Hypothesis for logistic regression is computed properly for two variables X', {
@@ -56,7 +56,7 @@ test_that('Hypothesis for logistic regression is computed properly for two varia
   theta <- c(1, 2, 3)
   expected <- sigmoid(t(theta) %*% t(X))
   expect_equal(as.vector(expected),
-               hypothesis(theta, X))
+               hyp_log_reg(theta, X))
 })
 
 
@@ -65,7 +65,7 @@ context('Cost for expected success')
 test_that('Cost is close to 0', {
   X <- matrix(c(1, 20, 1, 19), nrow = 2, byrow = T)
   th <- c(0, 1)
-  costt <- -log(hypothesis(th, X))
+  costt <- -log(hyp_log_reg(th, X))
   
   expect_equal(costt,
                cost_success(th, X))
@@ -74,7 +74,7 @@ test_that('Cost is close to 0', {
 test_that('Cost is aproaching Inf', {
   X <- matrix(c(1, -800, 1, -1000), nrow = 2, byrow = T)
   th <- c(0, 1)
-  costt <- -log(hypothesis(th, X))
+  costt <- -log(hyp_log_reg(th, X))
   
   expect_equal(costt,
                cost_success(th, X))
@@ -83,7 +83,7 @@ test_that('Cost is aproaching Inf', {
 test_that('Cost is between 0 and Inf', {
   X <- matrix(c(1, -800, 1, -1000), nrow = 2, byrow = T)
   th <- c(0, 0)
-  costt <- -log(hypothesis(th, X))
+  costt <- -log(hyp_log_reg(th, X))
   
   expect_equal(costt,
                cost_success(th, X))
@@ -95,7 +95,7 @@ context('Cost for expected failure')
 test_that('Cost is aproaching 0', {
   X <- matrix(c(1, -100, 1, -70), nrow = 2, byrow = T)
   th <- c(0, 1)
-  costt <- -log(1 - hypothesis(th, X))
+  costt <- -log(1 - hyp_log_reg(th, X))
   
   expect_equal(costt,
                cost_failure(th, X))
@@ -104,7 +104,7 @@ test_that('Cost is aproaching 0', {
 test_that('Cost is aproaching Inf', {
   X <- matrix(c(1, 200, 1, 190), nrow = 2, byrow = T)
   th <- c(0, 1)
-  costt <- -log(1 - hypothesis(th, X))
+  costt <- -log(1 - hyp_log_reg(th, X))
   
   expect_equal(costt,
                cost_failure(th, X))
@@ -113,7 +113,7 @@ test_that('Cost is aproaching Inf', {
 test_that('Cost is between 0 and Inf', {
   X <- matrix(c(1, -3, 1, -0.5), nrow = 2, byrow = T)
   th <- c(0, 1)
-  costt <- -log(1 - hypothesis(th, X))
+  costt <- -log(1 - hyp_log_reg(th, X))
   
   expect_equal(costt,
                cost_failure(th, X))
@@ -127,21 +127,21 @@ test_that('Only positive cases are computed properly', {
   
   X <- matrix(c(1, 20, 1, 19), nrow = 2, byrow = T)
   th <- c(0, 1)
-  costt <- 1 / nrow(X) * sum(-log(hypothesis(th, X)))
+  costt <- 1 / nrow(X) * sum(-log(hyp_log_reg(th, X)))
   
   expect_equal(costt,
                cost(th, X, y))
   
   X <- matrix(c(1, -8, 1, -10), nrow = 2, byrow = T)
   th <- c(0, 1)
-  costt <- 1 / nrow(X) * sum(-log(hypothesis(th, X)))
+  costt <- 1 / nrow(X) * sum(-log(hyp_log_reg(th, X)))
   
   expect_equal(costt,
                cost(th, X, y))
   
   X <- matrix(c(1, -8, 1, -10), nrow = 2, byrow = T)
   th <- c(0, 0)
-  costt <- 1 / nrow(X) * sum(-log(hypothesis(th, X)))
+  costt <- 1 / nrow(X) * sum(-log(hyp_log_reg(th, X)))
   
   expect_equal(costt,
                cost(th, X, y))
@@ -152,21 +152,21 @@ test_that('Only negative cases are computed properly', {
   
   X <- matrix(c(1, -10, 1, -7), nrow = 2, byrow = T)
   th <- c(0, 1)
-  costt <- 1 / nrow(X) * sum(-log(1 - hypothesis(th, X)))
+  costt <- 1 / nrow(X) * sum(-log(1 - hyp_log_reg(th, X)))
   
   expect_equal(costt,
                cost(th, X, y))
   
   X <- matrix(c(1, 20, 1, 19), nrow = 2, byrow = T)
   th <- c(0, 1)
-  costt <- 1 / nrow(X) * sum(-log(1 - hypothesis(th, X)))
+  costt <- 1 / nrow(X) * sum(-log(1 - hyp_log_reg(th, X)))
   
   expect_equal(costt,
                cost(th, X, y))
   
   X <- matrix(c(1, -3, 1, -0.5), nrow = 2, byrow = T)
   th <- c(0, 1)
-  costt <- 1 / nrow(X) * sum(-log(1 - hypothesis(th, X)))
+  costt <- 1 / nrow(X) * sum(-log(1 - hyp_log_reg(th, X)))
   
   expect_equal(costt,
                cost(th, X, y))
@@ -178,52 +178,54 @@ test_that('Mixed cases are computed properly', {
   X <- cbind(1, X)
   th <- c(1, 1)
   
-  expected <- 1 / nrow(X) * sum(-t(y) %*% log(hypothesis(th, X)) - t(1 - y) %*% log(1 - hypothesis(th, X)))
+  expected <- 1 / nrow(X) * sum(-t(y) %*% log(hyp_log_reg(th, X)) - t(1 - y) %*% log(1 - hyp_log_reg(th, X)))
   
   expect_equal(expected,
                cost(th, X, y))
 })
 
+context('Residuals')
 
-# test_that('Cost function returns cost between 0 and Inf', {
-#   X <- matrix(c(2, 0.4), nrow = 2)
-#   X <- cbind(1, X)
-#   y <- c(1, 0)
-#   th <- c(0, 1)
-#   h <- hypothesis(th, X)
-#   
-#   co <- 1 / nrow(X) * (-t(y) %*% log(h) - t(1 - y) %*% log(1 - h))
-#   expect_equal(co,
-#                cost(th, X, y))
-#   
-# })
-# 
-# test_that('Cost function returns cost equal close to 0', {
-#   X <- matrix(c(20, 19), nrow = 2)
-#   X <- cbind(1, X)
-#   y <- c(1, 1)
-#   th <- c(0, 1)
-#   h <- hypothesis(th, X)
-#   
-#   co <- 1 / nrow(X) * (-t(y) %*% log(h) - t(1 - y) %*% log(1 - h))
-#   expect_equal(co,
-#                cost(th, X, y))
-#   
-# })
-# 
-# test_that('Cost function returns cost equal Inf', {
-#   X <- matrix(c(-100, -200), nrow = 2)
-#   X <- cbind(1, X)
-#   y <- c(1, 1)
-#   th <- c(0, 1)
-#   h <- hypothesis(th, X)
-#   
-#   co <- 1 / nrow(X) * (-t(y) %*% log(h) - t(1 - y) %*% log(1 - h))
-#   expect_equal(co,
-#                cost(th, X, y))
-#   
-# })
-# 
-# test_that('Cost is computed properly for two independent variables', {
-#   
-# })
+test_that('Residuals for one independent variable in X are computed properly', {
+  X <- matrix(c(2, -2), nrow = 2)
+  X <- cbind(1, X)
+  y <- c(0, 1)
+  th <- c(2, 3)
+  r <- hyp_log_reg(th, X) - y
+  
+  expect_equal(r,
+               res(th, X, y, hyp_log_reg))
+})
+
+test_that('Residuals for two independent variables in X are computed properly', {
+  X <- matrix(c(2, -2, -3, 4), nrow = 2)
+  X <- cbind(1, X)
+  y <- c(0, 1)
+  th <- c(2, 3, 0.5)
+  r <- hyp_log_reg(th, X) - y
+  
+  expect_equal(r,
+               res(th, X, y, hyp_log_reg))
+})
+
+
+context('Gradient descent')
+
+test_that('Gradient descent is computed properly', {
+  X <- matrix(c(2, -2, -3, 4), nrow = 2)
+  X <- cbind(1, X)
+  y <- c(0, 1)
+  th <- c(2, 3, 0.5)
+  alpha <- 0.01
+  num_it <- 3
+  
+  c_vals <- numeric(num_it)
+  
+  for (i in 1:num_it) {
+    c_vals[i] <- cost(th, X, y)
+    th <- th - alpha / nrow(X) * t(X) %*% res(th, X, y, hyp_log_reg)
+  }
+  
+  expect_equal(gradient_descent(c(2, 3, 0.5), X, y, alpha, num_it, hyp_log_reg),
+               list(theta = th, cost_vals = c_vals))
+})
