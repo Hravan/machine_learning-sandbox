5ab7b922201d870ab89c06ef39bc0cecf08396b1
Krzysztof Krenc <31450069+Hravan@users.noreply.github.com>
2018-04-21 10:43:54 +0200

    Added stub and test for multiclass logistic regression

---
+++ logistic_regression/multiclass_classification.R
@@ -0,0 +1,14 @@
+multiclass_classification <- function(X, y) {
+  # Perform multiclass clasification One-vs-all algorithm
+  #
+  # Args:
+  #   X: m X n matrix of m observations of n independent variables
+  #   y: vector of m observations of the dependent variable
+  #
+  # Returns:
+  #   function that given a new observation classifies it to one of groups specified in y
+  
+  function(new_obs) {
+    0
+  }
+}
