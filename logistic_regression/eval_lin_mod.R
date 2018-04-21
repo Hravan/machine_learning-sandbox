bf955308cfa3fbf27404ad50a618e782d339aa7c
Krzysztof Krenc <31450069+Hravan@users.noreply.github.com>
2018-04-21 22:23:15 +0200

    eval_lin_mod uses many linear models at a time.

--- logistic_regression/eval_lin_mod.R
+++ logistic_regression/eval_lin_mod.R
@@ -1,12 +1,13 @@
-eval_lin_mod <- function(X, model) {
+eval_lin_mod <- function(X, models) {
   # Computes a predicted value of the dependend variable given a particular linear model
   #
   # Args:
   #   X:      matrix of observations
-  #   model:  vector of coefficients
+  #   model:  matrix of models
+  #           every column represents coefficients for one linear model
   #   ASSUMES: ncol(X) equals length(model)
   #
   # Returns:
-  #   vector of predicted values, one for each observation in X
-  as.vector(X %*% model)
+  #   matrix of predicted values, one row for each model for every observation in X
+  t(models) %*% t(X)
 }
--- logistic_regression/test_eval_lin_mod.R
+++ logistic_regression/test_eval_lin_mod.R
@@ -4,8 +4,8 @@ source('eval_lin_mod.R')
 
 context('Evaluate linear model')
 
-test_that('Predicted value is computed correctly for a given linear model', {
-  m1 <- c(2, 3, 4)
+test_that('Predicted value is computed correctly for one given linear model', {
+  m1 <- as.matrix(c(2, 3, 4))
   X <- matrix(c(2, 3,
                 4, 5,
                 7, 8),
@@ -13,9 +13,10 @@ test_that('Predicted value is computed correctly for a given linear model', {
               byrow = T)
   X <- cbind(1, X)
   
-  result <- c(2 + 3 * 2 + 4 * 3,
-              2 + 3 * 4 + 4 * 5,
-              2 + 3 * 7 + 4 * 8)
+  result <- matrix(c(2 + 3 * 2 + 4 * 3,
+                     2 + 3 * 4 + 4 * 5,
+                     2 + 3 * 7 + 4 * 8),
+                   nrow = 1)
   
   expect_equal(eval_lin_mod(X, m1),
                result)
@@ -28,9 +29,53 @@ test_that('Predicted value is computed correctly for a given linear model', {
               byrow = T)
   X <- cbind(1, X)
   
-  result <- c(2 + 3 * 2 + 4 * 3 + 5 * 1,
-              2 + 3 * 4 + 4 * 5 + 5 * 6,
-              2 + 3 * 7 + 4 * 8 + 5 * 9)
+  result <- matrix(c(2 + 3 * 2 + 4 * 3 + 5 * 1,
+                     2 + 3 * 4 + 4 * 5 + 5 * 6,
+                     2 + 3 * 7 + 4 * 8 + 5 * 9),
+                   nrow = 1)
+  
+  expect_equal(eval_lin_mod(X, m2),
+               result)
+})
+
+test_that('Predicted values are computed properly for many linear models', {
+  m1 <- matrix(c(2, 5,
+                 3, 6,
+                 4, 7),
+               nrow = 3,
+               byrow = T)
+  
+  X <- matrix(c(2, 3,
+                4, 5,
+                7, 8),
+              nrow = 3,
+              byrow = T)
+  X <- cbind(1, X)
+  
+  result <- matrix(c(2 + 3 * 2 + 4 * 3,
+                     2 + 3 * 4 + 4 * 5,
+                     2 + 3 * 7 + 4 * 8,
+                     5 + 6 * 2 + 7 * 3,
+                     5 + 6 * 4 + 7 * 5,
+                     5 + 6 * 7 + 7 * 8),
+                   nrow = 2,
+                   byrow = T)
+  
+  expect_equal(eval_lin_mod(X, m1),
+               result)
+  
+  m2 <- c(2, 3, 4, 5)
+  X <- matrix(c(2, 3, 1,
+                4, 5, 6,
+                7, 8, 9),
+              nrow = 3,
+              byrow = T)
+  X <- cbind(1, X)
+  
+  result <- matrix(c(2 + 3 * 2 + 4 * 3 + 5 * 1,
+                     2 + 3 * 4 + 4 * 5 + 5 * 6,
+                     2 + 3 * 7 + 4 * 8 + 5 * 9),
+                   nrow = 1)
   
   expect_equal(eval_lin_mod(X, m2),
                result)
